f22e26f2bc94707c1243c8f1db06c483fd134cdc
Cedrouseroll omondi <omondicedo@gmail.com>
2023-09-04 07:59:14 +0300

    feat: add layout to generated routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { setupLayouts } from 'virtual:meta-layouts'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto/routes'
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
+  routes: setupLayouts(routes)
 })
 
 export default router
