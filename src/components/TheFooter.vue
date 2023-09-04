1c3df34e6e206bce1d1afbdffc15b61e5e265295
Cedrouseroll omondi <omondicedo@gmail.com>
2023-09-04 08:01:39 +0300

    fix: footer overflows page content in mobile display

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <VFooter :elevation="24" class="d-none d-md-flex flex-column justify-center align-center mx-md-16 px-md-16 mb-md-5">
-    <a href="tel:0795086389" class="footer-link">+254 795 086 389</a>
+  <VFooter :elevation="24" class="d-flex flex-column text-center mt-16 mx-md-16 mb-md-5">
+    <a href="tel:0795086389" class="footer-link">+254 791 256 490</a>
     <a href="mailto:omondicedo@gmail.com" class="footer-link">omondicedo@gmail.com</a>
     <p>&copy; {{ new Date().getFullYear() }} Cedrouseroll Omondi. All rights reserved.</p>
   </VFooter>
