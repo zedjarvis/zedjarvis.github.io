2025f77e25b0460a50f2e60b9a052c479472f698
Cedrouseroll omondi <omondicedo@gmail.com>
2023-10-04 03:13:24 +0300

    update about text

--- components/About.vue
+++ components/About.vue
@@ -8,8 +8,8 @@ const name = useLocalStorage('visitor-name', '')
 <template>
   <div class="font-space text-slate-400">
     <p class="mb-3">Hello<span class="capitalize">
-        {{ name ? ' ' + name : '' }}</span>, I'm thrilled that you are here. I Hope our interaction goes beyond these 2
-      Paragraphs. I am a Developer from
+        {{ name ? ' ' + name : '' }}</span>, I'm delighted to have you here, and I hope our interaction goes far beyond
+      these two paragraphs. I am a Passionate Developer based in
       <span class="group/kenya inline-flex lg:cursor-[url('/images/cursor.png'),_pointer] lg:font-medium text-slate-200">
         <span class="sr-only">Kenya</span>
         <span class="group-hover/kenya:text-black transition duration-75 group-hover/kenya:-translate-y-px delay-[50ms]"
@@ -25,24 +25,19 @@ const name = useLocalStorage('visitor-name', '')
           class="group-hover/kenya:text-[#008c51] transition duration-75 group-hover/kenya:-translate-y-px delay-[150ms]"
           aria-hidden="true">a</span>
       </span>.
-      I run my first line of code on QPython in 2018. How did I get there? ...Short story; literaly - And I'm glad that I
+      I run my first line of code on QPython in 2018. How did I get there?
+      <InlineLink text="- Read More" url="#" /> ...Short story; literaly - And I'm glad that I
       did.
     </p>
-    <p class="mb-3">Fast-foward to today,I had the plasure of interacting
-      into the rabbit hole of coding and web development. Fast-forward to today, and I’ve had the privilege of
-      building software for an <a class="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
-        href="https://us.mullenlowe.com/" target="_blank" rel="noreferrer">advertising agency</a>, a<!-- --> <a
-        class="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300" href="https://starry.com/"
-        target="_blank" rel="noreferrer">start-up</a>, a<!-- --> <a
-        class="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
-        href="https://scout.camd.northeastern.edu/" target="_blank" rel="noreferrer">student-led design studio</a>,
-      and a<!-- --> <a class="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
-        href="https://www.apple.com/apple-music/" target="_blank" rel="noreferrer">huge corporation</a>.
+    <p class="mb-3">
+      Ever since then, I've had the privilege of working on a diverse range of projects spanning multiple technologies
+      including Python, PHP, C, C++, JavaScript, TypeScript, and powerful Frameworks like Django, React,
+      Vue.js, Svelte, Laravel, and more.
     </p>
     <p class="mb-3">
-      The most important thing I've learnt in this constantly evolving world of tech is that there is so much to learn,
-      but that's the best part cause I'm always ready to learn. Get in touch if you have a project, an opportunity or just
-      want to Connect.
+      One vital lesson I've learned in this ever-evolving tech landscape is that there's always something new to explore,
+      but that's the best part cause I'm always eager to embrace the challenge of learning. Whether you have an exciting
+      project in mind or simply want to connect, I'm just a message away.
     </p>
     <p>
       <RoughNotation :is-show="isShow" type="underline" color="#f4f169">
