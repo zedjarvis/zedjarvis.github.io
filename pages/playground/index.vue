59b85e3d88e8c859a46613ae025b561241dc1be3
Cedrouseroll Omondi <omondicedo@gmail.com>
2023-10-30 16:30:23 +0300

    Added 3d flying dragon

--- pages/playground/index.vue
+++ pages/playground/index.vue
@@ -1,59 +1,70 @@
 <script setup lang="ts">
-import { OrbitControls } from '@tresjs/cientos';
-import { TresCanvas, useRenderLoop } from '@tresjs/core';
+import { MouseParallax, OrbitControls, useAnimations, useGLTF } from '@tresjs/cientos';
+import { TresCanvas } from '@tresjs/core';
+import { breakpointsTailwind } from '@vueuse/core';
 import { BasicShadowMap, NoToneMapping, SRGBColorSpace } from 'three';
-import { reactive, ref } from 'vue';
+import { reactive } from 'vue';
 
-// const scene =  useFBX('/images/Lowpoly_Notebook_2.fbx')
 
 const state = reactive({
-  clearColor: '#201919',
+  clearColor: '#002937',
   shadows: true,
-  alpha: false,
-
+  alpha: true,
+  antialias: true,
   shadowMapType: BasicShadowMap,
   outputColorSpace: SRGBColorSpace,
   toneMapping: NoToneMapping,
 })
 
-const sphereRef = ref()
 
-const { onLoop } = useRenderLoop()
+const { scene, animations } = await useGLTF('/dragon_animation_flying/output.gltf', { draco: true })
 
-onLoop(({ elapsed }) => {
-  if (!sphereRef.value) return
-  sphereRef.value.position.y += Math.sin(elapsed) * 0.01
-})
+const breakpoints = useBreakpoints(breakpointsTailwind)
 
-function onPointerEnter(ev: any) {
-  if (ev) {
-    ev.object.material.color.set('#DFFF45')
-  }
-}
+const { actions } = useAnimations(animations, scene)
+
+let currentAction = actions.flying
+
+currentAction.play()
+
 </script>
 
 <template>
-  <div class="container flex justify-center items-center">
-
-    <div class="w-[600px] h-[400px] absolute top-52">
-      <TresCanvas v-bind="state">
-        <TresPerspectiveCamera :position="[5, 5, 5]" :fov="45" :near="0.1" :far="1000" :look-at="[0, 0, 0]" />
-        <OrbitControls />
-        <TresAmbientLight :intensity="0.5" />
-
-        <TresMesh ref="sphereRef" :position="[0, 4, 0]" cast-shadow @pointer-enter="onPointerEnter">
-          <TresSphereGeometry :args="[2, 32, 32]" />
-          <TresMeshToonMaterial color="teal" />
-        </TresMesh>
-
-        <TresDirectionalLight :position="[0, 8, 4]" :intensity="0.7" cast-shadow />
-        <TresMesh :rotation="[-Math.PI / 2, 0, 0]" receive-shadow>
-          <TresPlaneGeometry :args="[10, 10, 10, 10]" />
-          <TresMeshToonMaterial />
-        </TresMesh>
-
-        <TresDirectionalLight :position="[0, 2, 4]" :intensity="1" cast-shadow />
-      </TresCanvas>
-    </div>
+  <div class="h-16 py-5 px-28 w-full z-[999]">
+    <NuxtLink to="/"
+      class="group mb-2 inline-flex items-center font-semibold leading-tight text-teal-300 absolute top-0 left-0">
+      <UIcon name="i-carbon-arrow-left" class="mr-1 h-4 w-4 transition-transform group-hover:-translate-x-2" />
+      Cedrouseroll Omondi
+    </NuxtLink>
   </div>
+  <TresCanvas v-bind="state" window-size>
+    <TresPerspectiveCamera :position="[5, 2, 4]" :fov="20" />
+    <OrbitControls :enable-zoom="true" />
+    <TresHemisphereLight :intensity="0.15" ground-color="black" />
+    <TresSpotLight :position="[-20, 50, 10]" :angle="0.12" :penumbra="1" :intensity="1" cast-shadow
+      :shadow-mapsize="1024" />
+    <TresPointLight :intensity="1" />
+    <Suspense>
+      <primitive :object="scene" :scale="isMobile ? 0.7 : 1" :position="isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]"
+        :rotation="[-0.01, -0.2, -0.1]" />
+      <template #fallback>
+        <p>Loading...</p>
+      </template>
+    </Suspense>
+    <MouseParallax :factor="1" :ease="3" />
+    <TresAmbientLight :position="[10, 10, 10]" :intensity="1.5" color="red" cast-shadow />
+    <!-- <TresDirectionalLight :position="[0, 2, 4]" :intensity="1" cast-shadow /> -->
+  </TresCanvas>
 </template>
+
+<style scoped>
+#canvas {
+  width: 100vw;
+  height: 80vh;
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -9999;
+}
+</style>
