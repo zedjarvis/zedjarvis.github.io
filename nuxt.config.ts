cf54e3693c6e2921ab0dd084ff61530bb2a289bd
zedjarvis <omondicedo@gmail.com>
2024-06-28 00:02:05 +0300

    refactor: replace deprecated nuxt-simple-sitemap with @nuxtjs/sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
@@ -7,46 +6,53 @@ export default defineNuxtConfig({
   },
   ssr: false,
   modules: [
-    '@nuxt/ui',
-    '@nuxtjs/color-mode',
-    'nuxt-typed-router',
-    '@vueuse/nuxt',
-    '@vueuse/motion/nuxt',
-    'nuxt-gtag',
-    'nuxt-simple-sitemap',
-    'nuxt-simple-robots',
-    'nuxt-og-image',
-    'nuxt-schema-org',
-    '@tresjs/nuxt',
+    "@nuxt/ui",
+    "@nuxtjs/color-mode",
+    "nuxt-typed-router",
+    "@vueuse/nuxt",
+    "@vueuse/motion/nuxt",
+    "nuxt-gtag",
+    "nuxt-simple-robots",
+    "nuxt-og-image",
+    "nuxt-schema-org",
+    "@tresjs/nuxt",
+    "@nuxtjs/sitemap",
   ],
   ui: {
-    icons: ['carbon'],
+    icons: ["carbon"],
   },
   gtag: {
-    id: 'G-65E8J8GCZ5',
+    id: "G-65E8J8GCZ5",
   },
   site: {
-    url: 'https://zedjarvis.github.io'
+    url: "https://zedjarvis.github.io",
   },
-  css: ['~/assets/styles/main.css',],
+  css: ["~/assets/styles/main.css"],
   colorMode: {
-    classSuffix: ''
+    classSuffix: "",
   },
   app: {
     head: {
-      viewport: 'width=device-width,initial-scale=1',
+      viewport: "width=device-width,initial-scale=1",
       link: [
-        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: "icon", href: "/favicon.ico", sizes: "any" },
+        { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
       ],
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'Cedrouseroll Omondi is a Full-Stack Software Developer. He is very passionate about software development. His Primary coding language is Python. He has hands-on Professional Experience with using Django, Vuejs, React, Laravel, Postgresql, MongoDB, UnoCSS and Vuetify. When he is not coding, he is somewhere building custom furniture or making 3d designs with blender and Sketchup.' },
-        { name: 'apple-mobile-web-app-capable', content: 'yes' },
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-        { name: 'theme-color', content: '#1a3a46' },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content:
+            "Cedrouseroll Omondi is a Full-Stack Software Developer. He is very passionate about software development. His Primary coding language is Python. He has hands-on Professional Experience with using Django, Vuejs, React, Laravel, Postgresql, MongoDB, UnoCSS and Vuetify. When he is not coding, he is somewhere building custom furniture or making 3d designs with blender and Sketchup.",
+        },
+        { name: "apple-mobile-web-app-capable", content: "yes" },
+        {
+          name: "apple-mobile-web-app-status-bar-style",
+          content: "black-translucent",
+        },
+        { name: "theme-color", content: "#1a3a46" },
       ],
-      title: 'Cedrouseroll Omondi'
+      title: "Cedrouseroll Omondi",
     },
   },
-})
+});
